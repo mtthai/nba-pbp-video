5e0920d1045e689e00b3de62f2dc6db8b69e632a
mtthai <marvinthai@yahoo.com>
2019-10-15 22:22:07 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const axios = require('axios');
+const stat_endpoints = require('./endpoint.json');
+const jsonp = require('jsonp');
+
+var default_options = {formatted: true, parameters: false}
+
+generateURL = (params, endpoint) => {
+	var values = ({...stat_endpoints[endpoint].params, ...params});
+	var url = stat_endpoints[endpoint].url + "?";
+	var param_names = Object.keys(values);
+	
+	for(var i = 0; i < param_names.length; i++){
+		url = url + param_names[i] + "=" + values[param_names[i]] + "&";  
+	}
+
+	return url;
+}
+
+formatData = (json, options) => {
+
+	var data = {};
+	var parameters = json.parameters;
+
+	if(options.formatted){
+		var result_set = json.resultSets;
+		for(i in result_set){
+			var merged = {};
+			if(result_set[i].rowSet.length !== 1){
+				var multipleRowSets = {};
+				for(j in result_set[i].rowSet){
+					var temp = {};
+					for(k in result_set[i].headers){
+						temp[result_set[i].headers[k]] = result_set[i].rowSet[j][k];
+					}
+					multipleRowSets[j] = temp;
+				}
+				data[result_set[i].name] = multipleRowSets;	
+			} else {
+				for(j in result_set[i].headers){
+					merged[result_set[i].headers[j]] = result_set[i].rowSet[0][j];
+				}
+				data[result_set[i].name] = merged;
+			}
+		}
+	} else data = json;
+	if (options.parameters) return {data, parameters};
+	else return data;
+}
+
+getDataFromNBA = (params, endpoint, options) => {
+	var headers = {
+		"Accept-Encoding": "Accepflate, sdch",
+		"Accept-Language": "he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4",
+		"Cache-Control": "max-age=0",
+		Connection: "keep-alive",
+		Host: "stats.nba.com",
+		Referer: "http://stats.nba.com/",
+		"User-Agent":
+		"PostmanRuntime/7.4.0" 
+	};
+
+	var url = generateURL(params, endpoint);
+	
+	return new Promise(function(resolve, reject){
+		if(typeof window === 'undefined'){
+			axios.get(url, {headers}).then(function(res){
+				resolve(formatData(res.data, options))
+			}).catch(function(err){
+				reject(err)
+			});	
+		} else {
+			jsonp(url, null, (err, data) => {
+				if(!err) resolve(formatData(data, options))
+					else reject(err)
+				});
+		}
+	});
+}
+
+module.exports = {
+
+    playByPlay: function(params, options){
+		return getDataFromNBA(params, "play_by_play", {...default_options, ...options});
+	},
+
+	getPBPVideoURL: function(vid){
+		return new Promise(function(resolve, reject){
+			var url = 'https://stats.nba.com/stats/videoeventsasset?GameEventID=' + vid.EventNum + '&GameID=' + vid.GameID;
+			axios.get(url).then((res) => {
+			    var vidUrl = res.data.resultSets.Meta.videoUrls[0].lurl;
+				resolve(vidUrl);
+			}) 
+			.catch(function(err){
+				console.log(err);
+				reject(err)
+			});
+        })
+    }
+};
+
